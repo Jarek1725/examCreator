1df1845a58cb992df583aac9fd543e42f59ed4da
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-06-16 20:33:22 +0200

    first commit

---
+++ src/main/java/tomaszewskij/przedPraca/egzaminy/EgzaminyApplication.java
@@ -0,0 +1,13 @@
+package tomaszewskij.przedPraca.egzaminy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgzaminyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgzaminyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tomaszewskij/przedPraca/egzaminy/EgzaminyApplicationTests.java
@@ -0,0 +1,13 @@
+package tomaszewskij.przedPraca.egzaminy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgzaminyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
