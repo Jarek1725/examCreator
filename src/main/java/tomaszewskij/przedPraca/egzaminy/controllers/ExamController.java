fd942c06b04d03e80149cdef6094b445987d9586
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-06-30 18:53:54 +0200

    [*] Remove database to push on heroku

--- src/main/java/tomaszewskij/przedPraca/egzaminy/controllers/ExamController.java
+++ src/main/java/tomaszewskij/przedPraca/egzaminy/controllers/ExamController.java
@@ -1,9 +1,17 @@
 package tomaszewskij.przedPraca.egzaminy.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
+@RequestMapping("api/v1")
 public class ExamController {
 
+    @GetMapping("/test")
+    String test(){
+        return "TEST";
+    }
 }
