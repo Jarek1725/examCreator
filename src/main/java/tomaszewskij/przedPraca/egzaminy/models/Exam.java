b19f118bffa9a837b7548f2310787bb349467ddf
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-01 11:56:49 +0200

    Testing env variables

--- src/main/java/tomaszewskij/przedPraca/egzaminy/models/Exam.java
+++ src/main/java/tomaszewskij/przedPraca/egzaminy/models/Exam.java
@@ -1,38 +1,53 @@
 package tomaszewskij.przedPraca.egzaminy.models;
 
-//import javax.persistence.*;
+import javax.persistence.*;
 
-//@Entity(name = "Exam")
-//@Table(name = "exam")
+@Entity(name = "Exam")
+@Table(name = "exam")
 public class Exam {
-//    @Id
-//    @SequenceGenerator(
-//            name = "exam_sequence",
-//            sequenceName = "exam_sequence",
-//            allocationSize = 1
-//    )
-//    @GeneratedValue(
-//            strategy = GenerationType.SEQUENCE,
-//            generator = "exam_sequence"
-//    )
-//    @Column(
-//            name = "id",
-//            updatable = false
-//    )
+    @Id
+    @SequenceGenerator(
+            name = "exam_sequence",
+            sequenceName = "exam_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "exam_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
 
-//    @Column(
-//            name = "title",
-//            nullable = false,
-//            updatable = false
-//    )
+    @Column(
+            name = "title",
+            nullable = false,
+            updatable = false
+    )
     private String title;
+
+    @Column(
+            name = "test",
+            nullable = false,
+            updatable = false
+    )
+    private String test;
 
     public Exam() {
     }
 
     public Exam(String title) {
         this.title = title;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
     }
 
     public Long getId() {
