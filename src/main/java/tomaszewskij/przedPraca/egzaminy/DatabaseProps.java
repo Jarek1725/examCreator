0a956f784192a639bfb0536f5067651d55e597d4
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-01 12:39:57 +0200

    Testing env variables

--- src/main/java/tomaszewskij/przedPraca/egzaminy/DatabaseProps.java
+++ src/main/java/tomaszewskij/przedPraca/egzaminy/DatabaseProps.java
@@ -3,6 +3,6 @@ package tomaszewskij.przedPraca.egzaminy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("dbprops")
-public record DatabaseProps(String db_url, String db_login, String db_props) {
+public record DatabaseProps(String url, String login, String pass) {
 
 }
