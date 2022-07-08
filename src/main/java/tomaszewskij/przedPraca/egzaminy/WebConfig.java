66354c38d3cfc11095fed9bba14a5efa5a25d456
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-08 15:53:25 +0200

    Testing saving cookies

--- src/main/java/tomaszewskij/przedPraca/egzaminy/WebConfig.java
+++ src/main/java/tomaszewskij/przedPraca/egzaminy/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*")
+                .addMapping("/**").allowedOrigins("https://examcreator.herokuapp.com").allowedMethods("*").allowedHeaders("*")
                 .exposedHeaders("*").allowCredentials(true).maxAge(3600);
     }
 
