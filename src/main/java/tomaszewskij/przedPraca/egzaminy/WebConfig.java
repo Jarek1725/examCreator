bf0e3f3b989285832845733d4de1d801a2ead61d
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-18 18:55:45 +0200

    Add exam percentToPass property in graphql

--- src/main/java/tomaszewskij/przedPraca/egzaminy/WebConfig.java
+++ src/main/java/tomaszewskij/przedPraca/egzaminy/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
-                .addMapping("/**").allowedOrigins("https://examcreator.herokuapp.com").allowedMethods("*").allowedHeaders("*")
+                .addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*")
+//                .addMapping("/**").allowedOrigins("https://examcreator.herokuapp.com").allowedMethods("*").allowedHeaders("*")
                 .exposedHeaders("*").allowCredentials(true).maxAge(604800);
     }
 
